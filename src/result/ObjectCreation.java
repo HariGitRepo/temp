59cb7d95d42b0321cc27fa50437e95f62bc24a22
HariGitRepo <90850288+HariGitRepo@users.noreply.github.com>
2023-07-16 16:24:47 +0530

    object creationn

---
+++ src/result/ObjectCreation.java
@@ -0,0 +1,12 @@
+package result;
+
+public class ObjectCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ObjectCreation  obc = new ObjectCreation();
+		System.out.println("obc object created");
+	}
+
+}
